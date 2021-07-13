58067441a0c6650daa9bf222b59ab446317d4aad
daniel108 <D@NsP1993>
2021-07-12 21:37:06 -0300

    fix: label sized

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -28,77 +28,113 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Observer(builder: ( _) {
+      body: Observer(builder: (_) {
         return Container(
-            color: ColorsTheme.primary,
-            padding: EdgeInsets.all(16),
-            child: _controller.isLoading ?
-              Center(child: CircularProgressIndicator(semanticsLabel: Translations.of(context).text('loading'))) :
-                _controller.customer != null ?  Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    SizedBox(height: 50),
-                    Card(
-                      elevation: 4,
-                      child: ExpansionTile(
-                          initiallyExpanded: true,
-                          title: Text('${Translations.of(context).text('hello')}, ${_controller.customer!.name}', style: TextStyle(fontSize: 30)),
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(16),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Text('${Translations.of(context).text('balance')}:', style: TextStyle(fontSize: 23, color: Colors.black)),
-                                SizedBox(width: 8),
-                                Text('${MoneyFormat.changeToCurrent('R\$', _controller.customer!.balance)}', style: TextStyle(fontSize: 23, color: Colors.black)),
-                              ],),
-                          )
-                        ]),
-                    ),
-                    SizedBox(height: 8),
-                    Divider(color: Colors.white),
-                    Text(Translations.of(context).text('offers'), style: TextStyle(fontSize: 20, color: Colors.white)),
-                    Expanded(
-                      child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: _controller.customer!.offers.length,
-                            itemBuilder: (BuildContext context, index) {
-                              return InkWell(
-                                onTap: () async {
-                                  _controller.selectOffer(index);
-                                  Navigator.of(context).push(PageRouteAnimation.scale(OfferPage()));
-                                },
-                                child: Card(
-                                    elevation: 4,
-                                    child: Container(
-                                        padding: EdgeInsets.all(8),
-                                        height: 250,
-                                        child:
-                                  Column(
-                                    children: [
-                                      Expanded(flex: 3, child: PhotoHeroWidget(photo: _controller.customer!.offers[index].product!.image!)),
-                                      Expanded(
-                                        flex: 1,
-                                        child: Column(
-                                          children: [
-                                            Text('${_controller.customer!.offers[index].product!.name}', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18)),
-                                            Divider(),
-                                            Text('${MoneyFormat.changeToCurrent('R\$', _controller.customer!.offers[index].price)}', style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold)),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ))),
-                              );
-                            },
-                      ),
-                    ),
-                  ],
-            ) : Center(child: Text(Translations.of(context).text('no_customer'))),
-            );
-        }
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          color: ColorsTheme.primary,
+          padding: EdgeInsets.all(16),
+          child: _controller.isLoading
+              ? Center(
+              child: CircularProgressIndicator(
+                  semanticsLabel: Translations.of(context).text('loading')))
+              : _controller.customer != null
+              ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 50),
+              Card(
+                elevation: 4,
+                child: ExpansionTile(
+                    initiallyExpanded: true,
+                    title: Text(
+                        '${Translations.of(context).text('hello')}, ${_controller.customer!.name}',
+                        style: TextStyle(fontSize: 23)),
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                                '${Translations.of(context).text('balance')}:',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black)),
+                            SizedBox(width: 8),
+                            Text(
+                                '${MoneyFormat.changeToCurrent('R\$', _controller.customer!.balance)}',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black)),
+                          ],
+                        ),
+                      )
+                    ]),
+              ),
+              SizedBox(height: 8),
+              Divider(color: Colors.white),
+              Text(Translations.of(context).text('offers'),
+                  style:
+                  TextStyle(fontSize: 20, color: Colors.white)),
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: _controller.customer!.offers.length,
+                  itemBuilder: (BuildContext context, index) {
+                    return InkWell(
+                      onTap: () async {
+                        _controller.selectOffer(index);
+                        Navigator.of(context).push(
+                            PageRouteAnimation.scale(OfferPage()));
+                      },
+                      child: Card(
+                          elevation: 4,
+                          child: Container(
+                              padding: EdgeInsets.all(8),
+                              height: 250,
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                      flex: 3,
+                                      child: PhotoHeroWidget(
+                                          photo: _controller
+                                              .customer!
+                                              .offers[index]
+                                              .product!
+                                              .image!)),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                            '${_controller.customer!.offers[index].product!.name}',
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontWeight:
+                                                FontWeight.bold,
+                                                fontSize: 18)),
+                                        Divider(),
+                                        Text(
+                                            '${MoneyFormat.changeToCurrent('R\$', _controller.customer!.offers[index].price)}',
+                                            style: TextStyle(
+                                                color: Colors.green,
+                                                fontWeight:
+                                                FontWeight.bold)),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ))),
+                    );
+                  },
+                ),
+              ),
+            ],
+          )
+              : Center(
+              child:
+              Text(Translations.of(context).text('no_customer'))),
+        );
+      }), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/app/modules/offer/offer_page.dart
+++ lib/app/modules/offer/offer_page.dart
@@ -23,79 +23,89 @@ class _OfferPageState extends State<OfferPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(_controller.offer!.product!.name!), centerTitle: true),
-      body: Observer(builder: ( _) {
-        return _controller.offer != null ? SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Card(elevation: 4,
-                    child: Container(
+        appBar: AppBar(
+            title: Text(_controller.offer!.product!.name!), centerTitle: true),
+        body: Observer(
+          builder: (_) {
+            return _controller.offer != null
+                ? SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Card(
+                        elevation: 4,
+                        child: Container(
+                          padding: EdgeInsets.all(16),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                  '${Translations.of(context).text('balance')}:',
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.black)),
+                              SizedBox(width: 8),
+                              Text(
+                                  '${MoneyFormat.changeToCurrent('R\$', _homeController.customer!.balance)}',
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.black)),
+                            ],
+                          ),
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: PhotoHeroWidget(
+                          photo: _controller.offer!.product!.image!),
+                    ),
+                    Container(
                       padding: EdgeInsets.all(16),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text('${Translations.of(context).text('balance')}:', style: TextStyle(fontSize: 23, color: Colors.black)),
-                          SizedBox(width: 8),
-                          Text('${MoneyFormat.changeToCurrent('R\$', _homeController.customer!.balance)}', style: TextStyle(fontSize: 23, color: Colors.black)),
-                      ],),
-                    )),
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: PhotoHeroWidget(photo: _controller.offer!.product!.image!),
-                ),
-                Container(
-                  padding: EdgeInsets.all(16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(_controller.offer!.product!.name!, style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18)),
-                      SizedBox(height: 16),
-                      Text(_controller.offer!.product!.description!),
-                      SizedBox(height: 16),
-                      Divider(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('${MoneyFormat.changeToCurrent('R\$', _controller.offer!.price)}', style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold)),
-                          _controller.offer!.expired ?
-                          Text('${Translations.of(context).text('expired_at')}: ${DateUtil.convertToString(_controller.offer!.expireAt.toString(), 'HH:mm')}', style: TextStyle(fontWeight: FontWeight.bold)) :
-                          Text('${Translations.of(context).text('expire_at')}: ${DateUtil.convertToString(_controller.offer!.expireAt.toString(), 'HH:mm')}', style: TextStyle(fontWeight: FontWeight.bold))
-                        ],
-                      ),
-                      SizedBox(height: 16),
-                      Container(
-                        width: double.maxFinite,
-                        child: ElevatedButton(onPressed: () {
-                              if(_controller.offer!.expired) {
-                                showDialog(
-                                  context: context,
-                                  barrierDismissible: false,
-                                  builder: (context) {
-                                    return CustomDialog(
-                                      type: TypeDialog.WARNING,
-                                      title: Translations.of(context).text('oops'),
-                                      msg:
-                                      '${Translations.of(context).text('offer_expired_at')}: ${DateUtil.convertToString(_controller.offer!.expireAt.toString(), 'dd/MM/yyyy HH:mm')}',
-                                      btn1: true,
-                                      btn1Text: 'Ok',
-                                      isNavigation: false,
-                                    );
-                                  },
-                                );
-                              } else {
-                                _controller.buy(_homeController.customer!.balance!, _controller.offer!.price!).then((success) async {
-                                  if(success) {
-                                    _homeController.customer!.balance = _controller.balance;
+                          Text(_controller.offer!.product!.name!,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18)),
+                          SizedBox(height: 16),
+                          Text(_controller.offer!.product!.description!),
+                          SizedBox(height: 16),
+                          Divider(),
+                          Row(
+                            mainAxisAlignment:
+                            MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                  '${MoneyFormat.changeToCurrent('R\$', _controller.offer!.price)}',
+                                  style: TextStyle(
+                                      color: Colors.green,
+                                      fontWeight: FontWeight.bold)),
+                              _controller.offer!.expired
+                                  ? Text(
+                                  '${Translations.of(context).text('expired_at')}: ${DateUtil.convertToString(_controller.offer!.expireAt.toString(), 'HH:mm')}',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold))
+                                  : Text(
+                                  '${Translations.of(context).text('expire_at')}: ${DateUtil.convertToString(_controller.offer!.expireAt.toString(), 'HH:mm')}',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold))
+                            ],
+                          ),
+                          SizedBox(height: 16),
+                          Container(
+                            width: double.maxFinite,
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  if (_controller.offer!.expired) {
                                     showDialog(
                                       context: context,
                                       barrierDismissible: false,
                                       builder: (context) {
                                         return CustomDialog(
-                                          type: TypeDialog.OK,
-                                          title: '${Translations.of(context).text('success')}!!!',
+                                          type: TypeDialog.WARNING,
+                                          title: Translations.of(context)
+                                              .text('oops'),
                                           msg:
-                                          '${Translations.of(context).text('successful_purchase')}: ${MoneyFormat.changeToCurrent('R\$', _homeController.customer!.balance)}',
+                                          '${Translations.of(context).text('offer_expired_at')}: ${DateUtil.convertToString(_controller.offer!.expireAt.toString(), 'dd/MM/yyyy HH:mm')}',
                                           btn1: true,
                                           btn1Text: 'Ok',
                                           isNavigation: false,
@@ -103,36 +113,67 @@ class _OfferPageState extends State<OfferPage> {
                                       },
                                     );
                                   } else {
-                                    showDialog(
-                                      context: context,
-                                      barrierDismissible: false,
-                                      builder: (context) {
-                                        return CustomDialog(
-                                          type: TypeDialog.WARNING,
-                                          title: Translations.of(context).text('oops'),
-                                          msg:
-                                          '${Translations.of(context).text('not_have_balance')}: ${MoneyFormat.changeToCurrent('R\$', _homeController.customer!.balance)}',
-                                          btn1: true,
-                                          btn1Text: 'Ok',
-                                          isNavigation: false,
+                                    _controller
+                                        .buy(
+                                        _homeController
+                                            .customer!.balance!,
+                                        _controller.offer!.price!)
+                                        .then((success) async {
+                                      if (success) {
+                                        _homeController
+                                            .customer!.balance =
+                                            _controller.balance;
+                                        showDialog(
+                                          context: context,
+                                          barrierDismissible: false,
+                                          builder: (context) {
+                                            return CustomDialog(
+                                              type: TypeDialog.OK,
+                                              title:
+                                              '${Translations.of(context).text('success')}!!!',
+                                              msg:
+                                              '${Translations.of(context).text('successful_purchase')}: ${MoneyFormat.changeToCurrent('R\$', _homeController.customer!.balance)}',
+                                              btn1: true,
+                                              btn1Text: 'Ok',
+                                              isNavigation: false,
+                                            );
+                                          },
                                         );
-                                      },
-                                    );
+                                      } else {
+                                        showDialog(
+                                          context: context,
+                                          barrierDismissible: false,
+                                          builder: (context) {
+                                            return CustomDialog(
+                                              type: TypeDialog.WARNING,
+                                              title:
+                                              Translations.of(context)
+                                                  .text('oops'),
+                                              msg:
+                                              '${Translations.of(context).text('not_have_balance')}: ${MoneyFormat.changeToCurrent('R\$', _homeController.customer!.balance)}',
+                                              btn1: true,
+                                              btn1Text: 'Ok',
+                                              isNavigation: false,
+                                            );
+                                          },
+                                        );
+                                      }
+                                    });
                                   }
-                                });
-                              }
-
-                            }, child: Text(Translations.of(context).text('buy'))),
-                      )
-                    ],
-                  ),
+                                },
+                                child: Text(Translations.of(context)
+                                    .text('buy'))),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-
-              ],
-            ),
-          ),
-        ) : Container(child: Text(Translations.of(context).text('not_found')));
-      },
-    ));
+              ),
+            )
+                : Container(
+                child: Text(Translations.of(context).text('not_found')));
+          },
+        ));
   }
 }
